8fed77f22bf399c844a85edbd5c39097c1d72dae
Steve Tooke <steve@boxjump.co.uk>
2014-11-20 16:25:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YeasuRailsErrors::Application.config.secret_token = 'ddbdb3628c43d13311b79813bb0dfcfd07ad317ec62af20ffbe404d3261e10da2991e37639d4df70ba6b4112c5ea585f094ec0e6cd294b5809f97c7f7c3b158d'
